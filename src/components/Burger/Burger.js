e76d8b460a2f6e8a2914eeaa9a159157de17cf98
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-22 05:12:38 -0500

    Dynamically render ingredients based on the props

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,17 @@ import classes from './Burger.css';
 import Ingredient from './Ingredient/Ingredient'
 
 const Burger = (props) => {
+	const finalBurger = Object.entries(props.ingredients)
+		.map(ig => {
+			return [...Array(ig[1])].map((_, index) => {
+				return <Ingredient key={ig[0] + index} type={ig[0]} />
+			})
+		});
+
 	return(
 		<div className={classes.Container}>
 			<Ingredient type='breadTop' />
-			<Ingredient type='salad' />
-			<Ingredient type='meat' />
-			<Ingredient type='bacon' />
-			<Ingredient type='cheese' />
+			{finalBurger}
 			<Ingredient type='breadBottom' />
 		</div>
 	);
