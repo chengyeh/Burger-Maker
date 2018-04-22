6bcd15354b5b047106903e16a695b5c56ead560b
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-22 18:20:06 -0500

    Added state for ingredients

--- src/containers/BurgerMaker/BurgerMaker.js
+++ src/containers/BurgerMaker/BurgerMaker.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerMaker extends Component {
+	state = {
+				ingredients: {
+					salad: 0,
+					meat: 0,
+					bacon: 0,
+					cheese: 0,
+				}
+			};
 
 	render() {
 		return(
 			<React.Fragment>
-				<div>Burger</div>
+				<Burger ingredients={this.state.ingredients}/>
 				<div>Make controller</div>
 			</React.Fragment>
 		);
