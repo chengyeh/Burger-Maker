7ea5fe361d40c46dd82a2b75b054e251a96dff68
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-22 03:28:12 -0500

    Added Layout component template

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Layout from '../../components/Layout/Layout';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      	<p>Test</p>
+      	<Layout>
+      		<p>Test</p>
+      	</Layout>
       </div>
     );
   }
