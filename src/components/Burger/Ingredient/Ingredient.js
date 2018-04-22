c3c434ba9cc7c33b72e7fe4555ba1ee7e0e337e5
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-22 04:39:20 -0500

    Fixed the typo of css class name

--- src/components/Burger/Ingredient/Ingredient.js
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -8,27 +8,27 @@ const Ingredient = (props) => {
 
 	switch (props.type) {
 		case 'breadBottom':
-			ingredient = <div className='classes.BreadBottom'></div>;
+			ingredient = <div className={classes.BreadBottom}></div>;
 			break;
 		case 'breadTop':
 			ingredient = (
-				<div className='classes.BreadTop'>
-					<div className='classes.Seeds1'></div>
-					<div className='classes.Seeds2'></div>
+				<div className={classes.BreadTop}>
+					<div className={classes.Seeds1}></div>
+					<div className={classes.Seeds2}></div>
 				</div>
 			);
 			break;
 		case 'meat':
-			ingredient = <div className='classes.Meat'></div>;
+			ingredient = <div className={classes.Meat}></div>;
 			break;
 		case 'cheese':
-			ingredient = <div className='classes.Cheese'></div>;
+			ingredient = <div className={classes.Cheese}></div>;
 			break;
 		case 'salad':
-			ingredient = <div className='classes.Salad'></div>;
+			ingredient = <div className={classes.Salad}></div>;
 			break;
 		case 'bacon':
-			ingredient = <div className='classes.Bacon'></div>;
+			ingredient = <div className={classes.Bacon}></div>;
 			break;
 		default:
 			ingredient = null;
