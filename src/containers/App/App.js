e632ccb81694b4392e5d28b6610d8d7e448cf600
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-22 03:30:49 -0500

    Added BurgerMaker component template

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from '../../components/Layout/Layout';
+import BurgerMaker from '../BurgerMaker/BurgerMaker';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       	<Layout>
-      		<p>Test</p>
+      		<BurgerMaker />
       	</Layout>
       </div>
     );
---
+++ src/containers/BurgerMaker/BurgerMaker.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BurgerMaker extends Component {
+
+	render() {
+		return(
+			<React.Fragment>
+				<div>Burger</div>
+				<div>Make controller</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default BurgerMaker;
