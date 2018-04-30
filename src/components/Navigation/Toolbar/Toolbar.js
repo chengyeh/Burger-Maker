6716b9c3ce4622b0ac54005caaa4c5a3cde28de2
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-29 19:25:16 -0500

    Adjusted Logo component height

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavBar from '../NavBar/NavBar';
 
 const Toolbar = (props) => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
-		<Logo />
+		<div className={classes.Logo}>
+			<Logo />
+		</div>
 		<nav>
-			...
+			<NavBar />
 		</nav>
 	</header>
 );
