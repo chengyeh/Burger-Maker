dd8dc8c92563fe00479e25be419862a5dbb30056
Cheng Yeh Lee <chengyeh@ku.edu>
2018-04-30 04:10:43 -0500

    Display the backdrop when the sidedrawer is open, and close it by clicking the backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-	<React.Fragment>
-		<Toolbar />
-		<SideDrawer />
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</React.Fragment>
-);
+class Layout extends Component {
+	state = {
+		sideDrawerVisble: true
+	};
+
+	closeSideDrawerHandler = () => {
+		this.setState({sideDrawerVisble: false})
+	}
+
+	render() {
+		return(
+			<React.Fragment>
+				<Toolbar />
+				<SideDrawer 
+					show={this.state.sideDrawerVisble} 
+					closed={this.closeSideDrawerHandler}/>
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</React.Fragment>
+		);
+	}
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,28 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavBar from '../NavBar/NavBar';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const SideDrawer = (props) => {
+	const sideDrawerCss = [classes.SideDrawer];
+	if(props.show) {
+		sideDrawerCss.push(classes.Open)
+	} else {
+		sideDrawerCss.push(classes.Close)
+	}
+
 	return(
-		<div className={classes.SideDrawer}>
-			<div className={classes.Logo}>
-				<Logo />
+		<React.Fragment>
+			<Backdrop show={props.show} clicked={props.closed}/>
+			<div className={sideDrawerCss.join(' ')}>
+				<div className={classes.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavBar />
+				</nav>
 			</div>
-			<nav>
-				<NavBar />
-			</nav>
-		</div>
+		</React.Fragment>
 	);
 };
 
