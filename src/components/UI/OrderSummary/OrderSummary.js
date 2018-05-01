67714322cf1e8b7ed55219fa35a209e157315b95
Cheng Yeh Lee <chengyeh@ku.edu>
2018-05-01 02:51:41 -0500

    Added shouldComponentUpdate lifecycle method to prevent unnecessary re-render component

--- src/components/UI/OrderSummary/OrderSummary.js
+++ src/components/UI/OrderSummary/OrderSummary.js
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Button from '../Button/Button';
 import CurrencyFormat from '../../CurrencyFormat/CurrencyFormat';
 
-const OrderSummary = (props) => {
-	const listOfAddedIngredients = Object.entries(props.ingredients).filter(ing => {
-		return ing[1] > 0;
-	}).map(ing => {
+class OrderSummary extends Component {
+
+	shouldComponentUpdate(nextProps) {
+		return nextProps.ingredients !== this.props.ingredients;
+	}
+
+	render() {
+		const listOfAddedIngredients = Object.entries(this.props.ingredients).filter(ing => {
+			return ing[1] > 0;
+		}).map(ing => {
+			return(
+				<li key={ing[0]}>
+					<span style={{textTransform: 'capitalize'}}>{ing[0]}</span>: {ing[1]} <strong>(+ </strong>
+						<CurrencyFormat amount={this.props.ingsPrice[ing[0]] * ing[1]}/>)</li>
+			);
+		})
 		return(
-			<li key={ing[0]}>
-				<span style={{textTransform: 'capitalize'}}>{ing[0]}</span>: {ing[1]} <strong>(+ </strong>
-					<CurrencyFormat amount={props.ingsPrice[ing[0]] * ing[1]}/>)</li>
+			<React.Fragment>
+				<h3>Order Summary</h3>
+				<p>The following ingredients have been added to your burger:</p>
+				<ul>
+					{listOfAddedIngredients}
+					<li>
+						Base Price: <CurrencyFormat amount={this.props.basePrice} /></li>
+				</ul>
+				<hr/>
+				<p>Total Price: <CurrencyFormat amount={this.props.total} /></p>
+				<p>Continue to checkout?</p>
+				<Button btnType='Danger' clicked={this.props.cancelOrder}>Cancel</Button>
+				<Button btnType='Success' clicked={this.props.continue}>Continue</Button>
+			</React.Fragment>
 		);
-	})
-	
-	return(
-		<React.Fragment>
-			<h3>Order Summary</h3>
-			<p>The following ingredients have been added to your burger:</p>
-			<ul>
-				{listOfAddedIngredients}
-				<li>
-					Base Price: <CurrencyFormat amount={props.basePrice} /></li>
-			</ul>
-			<hr/>
-			<p>Total Price: <CurrencyFormat amount={props.total} /></p>
-			<p>Continue to checkout?</p>
-			<Button btnType='Danger' clicked={props.cancelOrder}>Cancel</Button>
-			<Button btnType='Success' clicked={props.continue}>Continue</Button>
-		</React.Fragment>
-	);
-};
+	}
+} 
 
 export default OrderSummary;
