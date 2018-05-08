b945dedd469f92f1e3a2652918b1fb909f6498a0
Cheng Yeh Lee <chengyeh@ku.edu>
2018-05-07 21:36:36 -0500

    Fetch orders from the database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-instances/order';
 
 class Orders extends Component {
+	state = {
+		orders: null,
+		loading: true
+	};
+
+	componentDidMount() {
+		const orders = [];
+		axios.get('/orders.json')
+			.then(response => {
+				 for(let key in response.data) {
+				 	orders.push({
+				 		...response.data[key],
+				 		id: key
+				 	});
+				 }
+				 this.setState({orders, loading: false});
+			})
+			.catch(err => {
+				this.setState({loading: false});
+			});
+	}
+
 	render() {
+		console.log(this.state)
 		return(
 			<div>
 				<Order />
