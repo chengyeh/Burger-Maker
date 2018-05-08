263d7b1fc22ef9f25db9b788f74f701c2ce87091
Cheng Yeh Lee <chengyeh@ku.edu>
2018-05-07 22:19:53 -0500

    Displaay list of ingredients and total fetched from the database

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,11 +3,28 @@ import React from 'react';
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat';
 import className from './Order.css'
 
-const Order = (props) => (
-	<div className={className.Order}>
-		<p>Ingredients: lettuce (1)</p>
-		<p>Total: <CurrencyFormat amount={7.25}/></p>
-	</div>
-);
+const Order = (props) => {
+	const ingredients = [];
+	for(let key in props.ingredients) {
+		ingredients.push({
+			name: key,
+			amount: props.ingredients[key]
+		})
+	}
+
+	const ingredientsList = ingredients.map(ing => {
+		return <span 
+				key={ing.name}
+				className={className.Ingredient}>{`${ing.name} (${ing.amount})`}</span>
+	})
+
+	return(
+		<div className={className.Order}>
+			<p>Ingredients: {ingredientsList}</p>
+			<p>Total: <CurrencyFormat amount={props.total}/></p>
+		</div>
+
+	);
+};
 
 export default Order;
