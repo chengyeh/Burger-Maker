d16a9f2f2baed59a070f6b3ad397f95f1181cd30
Cheng Yeh Lee <chengyeh@ku.edu>
2018-05-08 01:35:28 -0500

    Updated component to also accept element type select

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,15 +5,37 @@ import classes from './Input.css';
 const Input = (props) => {
 	let inputEl = null;
 
-	switch (props.inputtype) {
+	switch (props.eleType) {
 		case 'input':
-			inputEl = <input className={classes.InputEl} {...props} />;
+			inputEl = <input 
+						className={classes.InputEl} 
+						{...props.eleConfig} 
+						value={props.value} />;
 			break;
 		case 'textarea':
-			inputEl = <textarea className={classes.InputEl} {...props} />;
+			inputEl = <textarea 
+						className={classes.InputEl} 
+						{...props.eleConfig} 
+						value={props.value}/>;
+			break;
+		case 'select':
+			inputEl = (
+					<select 
+						className={classes.InputEl} 
+						value={props.value}>
+						{props.eleConfig.options.map(option => (
+							<option key={option.value} value={option.value}>
+								{option.displayValue}
+							</option>
+						))}
+					</select>
+					  );
 			break;
 		default:
-			inputEl = <input className={classes.InputEl} {...props} />;
+			inputEl = <input 
+						className={classes.InputEl} 
+						{...props.eleConfig} 
+						value={props.value}/>;
 
 	}
 	return(
